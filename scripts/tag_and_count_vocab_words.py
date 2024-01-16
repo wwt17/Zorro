a55361d3f77bf4bdee403be7df7b7c8be8d9c7f0
Wentao Wang <wentao.wang@nyu.edu>
2024-01-16 17:07:41 -0500

    process multiple corpora

--- scripts/tag_and_count_vocab_words.py
+++ scripts/tag_and_count_vocab_words.py
@@ -4,26 +4,29 @@ alongside their frequency in corpora of interest (e.g. childes, newsela, wikiped
 
 A huggingface tokenizers v0.10 configuration file is expected.
 """
+from typing import Optional
 import argparse
 import spacy
 from spacy.tokens import Doc
 import json
 from pathlib import Path
 import pandas as pd
+import tqdm
 from zorro import configs
 
 
 POS_TAGS = [
-    'NNP',  # proper noun
-    'NN' ,
-    'NNS',
-    'JJ' ,
-    'VB' ,  # base form of verb
-    'VBD',  # verb past tense
-    'VBG',  # verb gerund or present participle
-    'VBN',  # verb past participle
-    'VBP',  # verb non-3rd person singular present
-    'VBZ',  # verb 3rd person singular present
+    'CD'  ,  # cardinal number
+    'NN'  ,
+    'NNS' ,
+    'NNP' ,  # proper noun
+    'NNPS',
+    'VB'  ,  # base form of verb
+    'VBD' ,  # verb past tense
+    'VBG' ,  # verb gerund or present participle
+    'VBN' ,  # verb past participle
+    'VBP' ,  # verb non-3rd person singular present
+    'VBZ' ,  # verb 3rd person singular present
 ]
 
 
@@ -45,37 +48,42 @@ def is_excluded(w: str):
 def update_row(
         df: pd.DataFrame,
         idx: int,
-        tag: str,
+        tag: Optional[str],
         corpus_name: str,
 ):
-    try:
+    if tag is not None:
+        if tag not in df.columns:
+            df[tag] = 0
         df.loc[idx, tag] += 1 # type: ignore
-    except KeyError:
-        pass
     df.loc[idx, 'total-frequency'] += 1 # type: ignore
-    try:
-        df.loc[idx, f'{corpus_name}-frequency'] += 1 # type: ignore
-    except KeyError:
-        pass
+    df.loc[idx, f'{corpus_name}-frequency'] += 1 # type: ignore
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        "--tokenizer_file", type=Path,
-        help="Path to the HuggingFace tokenizer configuration file."
+        "--out_name", default="vocab",
+        help="The name of the output vocab file."
     )
     argparser.add_argument(
-        "--vocab_file", type=Path,
-        help="Path to the vocab file."
+        "--vocab_file", type=Path, nargs="*", default=[],
+        help="Path(s) to the vocab file(s)."
     )
     argparser.add_argument(
         "--corpora", type=Path, nargs="*", required=True,
         help="Paths to the corpora."
     )
     argparser.add_argument(
+        "--skip_pos_tag_corpora", default=["childes"],
+        help="Skip getting POS tag frequencies in these corpora (given by their names)."
+    )
+    argparser.add_argument(
         "--whole_word", action="store_true",
         help="Ensure remaining words to be whole words."
+    )
+    argparser.add_argument(
+        "--exclude_not_occurring_words", choices=["all", "any"], default="any",
+        help="Exclude words that do not occur in (any|all) corpora."
     )
     argparser.add_argument(
         "--dry_run", action="store_true",
@@ -85,20 +93,35 @@ if __name__ == "__main__":
     )
     args = argparser.parse_args()
 
-    if args.vocab_file is not None:
-        vocab_name = args.vocab_file.stem
-        with open(args.vocab_file) as f:
+    corpora = args.corpora
+    corpus_names = []
+    args.corpora = []
+    for corpus_path in corpora:
+        if corpus_path.is_dir():
+            corpus_name = corpus_path.name
+            args.vocab_file.append(corpus_path/"vocab.json")
+            args.corpora.append(corpus_path/"train.txt")
+        else:
+            corpus_name = corpus_path.stem
+            args.corpora.append(corpus_path)
+        corpus_names.append(corpus_name)
+    print(f'Will count vocab words in the following corpora:')
+    for c in args.corpora:
+        print(c)
+
+    merged_vocab = set()
+    for vocab_file in args.vocab_file:
+        vocab_name = vocab_file.stem
+        with open(vocab_file) as f:
             vocab = json.load(f)
-    elif args.tokenizer_file is not None:
-        vocab_name = args.tokenizer_file.stem
-        # get vocab from tokenizer, without space symbol
-        with open(args.tokenizer_file) as f:
-            tokenizer_data = json.load(f)
-        vocab = tokenizer_data['model']['vocab']
-    else:
-        raise Exception("Must provide either tokenizer_file or vocab_file.")
-    assert isinstance(vocab, dict)
-    assert max(vocab.values()) + 1 == len(vocab)
+        try:
+            vocab = vocab['model']['vocab']  # get vocab from tokenizer, without space symbol
+        except:
+            pass
+        assert isinstance(vocab, dict)
+        assert max(vocab.values()) + 1 == len(vocab)
+        merged_vocab |= vocab.keys()
+    vocab = {word: idx for idx, word in enumerate(merged_vocab)}
     vocab_no_space_symbol = {w.strip(configs.Data.space_symbol) for w in vocab}
     vocab_no_space_symbol = {
         w: idx for idx, w in enumerate(vocab_no_space_symbol)
@@ -113,44 +136,55 @@ if __name__ == "__main__":
     nlp.tokenizer = lambda text: Doc(nlp.vocab, words=text.split())
     #nlp.tokenizer.add_special_case("<unk>", [{spacy.symbols.ORTH: "<unk>"}])
 
-    print(f'Will count vocab words in the following corpora:')
-    for c in args.corpora:
-        print(c)
-
     # get information about all words in corpora
-    corpus_names = [c.stem for c in args.corpora]
     df = pd.DataFrame(
         0,
         index=range(len(vocab_no_space_symbol)),
         columns=(
-            POS_TAGS +
             ['total-frequency'] +
-            [f'{corpus_name}-frequency' for corpus_name in corpus_names]
+            [f'{corpus_name}-frequency' for corpus_name in corpus_names] +
+            POS_TAGS
         )
     )
     for corpus_path, corpus_name in zip(args.corpora, corpus_names):
         with open(corpus_path) as f:
             sentences = list(map(str.lower, map(str.strip, f.readlines())))
 
-        for n, sd in enumerate(nlp.pipe(sentences, disable=["parser", "ner"])):
-            for sw in sd:
-                update_row(
-                    df, vocab_no_space_symbol[sw.text], sw.tag_, corpus_name
-                )
+        if corpus_name in args.skip_pos_tag_corpora:
+            for sentence in tqdm.tqdm(sentences, desc=corpus_name):
+                words = sentence.split()
+                for word in words:
+                    update_row(
+                        df, vocab_no_space_symbol[word], None, corpus_name
+                    )
 
-            if args.dry_run and n+1 >= args.dry_run_n_sentences:
-                break
+        else:
+            for n, sd in enumerate(nlp.pipe(tqdm.tqdm(sentences, desc=corpus_name), disable=["parser", "attribute_ruler", "lemmatizer", "ner"])):
+                for sw in sd:
+                    update_row(
+                        df, vocab_no_space_symbol[sw.text], sw.tag_, corpus_name
+                    )
 
-            if n % 1000 == 0:
-                print(f'{corpus_path.stem:<24} {n:>12,}/{len(sentences):>12,}')
+                if args.dry_run and n+1 >= args.dry_run_n_sentences:
+                    break
 
     df.index: pd.core.indexes.base.Index = vocab_no_space_symbol.keys() # type: ignore
     df["is_excluded"] = df.index.map(is_excluded)
 
-    not_occurring = df["total-frequency"] == 0
-    tokens_not_in_corpus = df.index[not_occurring].to_list()
-    print(f'Did not find {len(tokens_not_in_corpus)} tokens from vocab in corpora: {", ".join(tokens_not_in_corpus)}')
-    df["is_excluded"] |= not_occurring
+    not_occurring_in_all = df["total-frequency"] == 0
+    tokens_not_in_all_corpora = df.index[not_occurring_in_all].to_list()
+    print(f'Did not find {len(tokens_not_in_all_corpora)} tokens from vocab in all corpora: {", ".join(tokens_not_in_all_corpora)}')
+
+    not_occurring_in_any = False
+    for corpus_name in corpus_names:
+        not_occurring_in_any |= df[f"{corpus_name}-frequency"] == 0
+    tokens_not_in_any_corpus = df.index[not_occurring_in_any].to_list()
+    print(f'Did not find {len(tokens_not_in_any_corpus)} tokens from vocab in any corpus: {", ".join(tokens_not_in_any_corpus)}')
+
+    df["is_excluded"] |= {
+        "all": not_occurring_in_all,
+        "any": not_occurring_in_any
+    }[args.exclude_not_occurring_words]
 
     df.sort_values(by='total-frequency', inplace=True, ascending=False)
 
@@ -158,4 +192,4 @@ if __name__ == "__main__":
         print(df)
         exit(f'Dry run completed after {args.dry_run_n_sentences} sentences')
 
-    df.to_csv(configs.Dirs.data / 'vocab_words' / f'{vocab_name}.csv', index=True)
+    df.to_csv(configs.Dirs.data / 'vocab_words' / f'{args.out_name}.csv', index=True)
